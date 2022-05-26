dbf4b661d263e41417b19a00077d8473c49246e8
sk1nny <yagoluiz28@gmail.com>
2022-05-25 22:09:47 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,22 @@ export default function Home() {
         </p>
     
       </main>
- 
+ <body>
+    <h1> teste </h1>
+    <script src="https://unpkg.com/blip-chat-widget" type="text/javascript"></script>
+
+<script>
+    (function () {
+        window.onload = function () {
+            new BlipChat()
+            .withAppKey('dHJhbnNib3JkbzQ1cGVnd3RzcWF5dWhqOXduYm06MWVlODhiNmUtMThlYS00ODlmLWI4MWYtMmI5ZWJhN2UxZjlk')
+            .withButton({"color":"#2CC3D5","icon":"https://blipmediastore.blob.core.windows.net/public-medias/Media_214b44af-a72b-4736-939e-3cf8c62e4332"})
+            .withCustomCommonUrl('https://yago-luiz-hillesheim-81lzw.chat.blip.ai/')
+            .build();
+        }
+    })();
+</script>
+                                
                                 
 
       <Footer />
